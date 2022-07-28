deb7bd4f2f35f0c67c34615ccb4417b2e1fd6c11
SubashAsusX515 <subash.barik@gmail.com>
2022-07-28 10:32:09 +0530

    Seed Data Inserted data in the tables during program startup.

--- API/Program.cs
+++ API/Program.cs
@@ -23,11 +23,11 @@ namespace API
             {
                 var services = scope.ServiceProvider;
                 var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-
                 try
                 {
                     var context = services.GetRequiredService<StoreContext>();
                     await context.Database.MigrateAsync();
+                    await StoreContextSeed.SeedAsync(context,loggerFactory);
                 }
                 catch(Exception ex)
                 {
