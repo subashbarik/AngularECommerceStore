c64439ce1b5d78d92931e4490c9b08b23156a17a
SubashAsusX515 <subash.barik@gmail.com>
2022-09-03 16:54:09 +0530

    Breadcrumb for shop module.

--- AngularClient/src/app/featured/shop/shop-routing.module.ts
+++ AngularClient/src/app/featured/shop/shop-routing.module.ts
@@ -2,11 +2,19 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductResolver } from './product.resolver';
 import { ShopComponent } from './shop.component';
 
 const routes: Routes = [
   { path: '', component: ShopComponent },
-  { path: ':id', component: ProductDetailsComponent },
+  {
+    path: ':id',
+    component: ProductDetailsComponent,
+    data: {
+      breadcrumb: (data: any) => `${data.product.name}`,
+    },
+    resolve: { product: ProductResolver },
+  },
 ];
 @NgModule({
   declarations: [],
